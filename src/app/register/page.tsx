050bc7156f9e340460c1d96a68f1cfd18388ad35
Danilo Ibanez <danipelis0311@gmail.com>
2024-02-02 13:45:22 -0500

    ADD-RegisterPage

---
+++ src/app/register/page.tsx
@@ -0,0 +1,18 @@
+import { montserrat } from "../ui/fonts";
+
+export default async function RegisterPage() {
+  
+  return (
+    <main>
+      <h1 className={`${montserrat.className} mb-4 text-xl md:text-2xl`}>
+        Registro
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        
+      </div>
+    </main>
+  )
+} 
